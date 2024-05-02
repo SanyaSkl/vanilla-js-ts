46ddbf1cc10183aaec3fdf8b3fca9de0ae4bd971
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-05-03 01:11:47 +0300

    vanilla-js lesson-02-02 and test 02

---
+++ src/lessons/lesson-02/lesson-02-02.ts
@@ -0,0 +1,29 @@
+ export type StreetType = {
+    title: string
+ }
+
+export type AddressType = {
+    number?: number
+     street: StreetType
+ }
+export type HouseType = {
+    builtAt: number
+     repaired: boolean
+     address: AddressType
+
+ }
+
+ type governmentBuildings = {
+     type: string
+     budget: number
+     staffCount: number
+     address: AddressType
+ }
+
+export type CityType = {
+    title: "HOSPITAL" | "FIRE-STATION" | "New York"
+     houses: Array<HouseType>
+     governmentBuildings: Array<governmentBuildings>
+     citizensNumber: number
+ }
+
--- src/lessons/lesson-02/lesson-02.ts
+++ src/lessons/lesson-02/lesson-02.ts
@@ -1,3 +1,56 @@
+type LocalCityType = {
+    title: string,
+    countryTitle: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type TechnologiesType = {
+    id: number
+    title: string
+}
+
+export type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechnologiesType>
+}
+
+export const student: StudentType = {
+    id: 1,
+    name: "Alexandr",
+    age: 31,
+    isActive: true,
+    address: {
+        streetTitle: "Sergievskaya 20",
+        city: {
+            title: "Staryy Oskol",
+            countryTitle: "Russia"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },
+        {
+            id: 3,
+            title: "React"
+        }
+    ]
+}
+
+
 type YearOfBirthType = {
     year: number
     month: string
@@ -10,7 +63,7 @@ type OwnerListType = {
     yearOfBirth: YearOfBirthType
 }
 
-type  CarListType  = {
+type  CarListType = {
     id: number
     brand: string
     model: string
