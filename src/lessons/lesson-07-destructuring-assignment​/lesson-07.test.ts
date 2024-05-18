ad20a7e578ebab8140eedff2885319c349c57c14
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-05-18 15:25:40 +0300

    lesson-07 destructuring of array

--- src/lessons/lesson-07-destructuring-assignment​/lesson-07.test.ts
+++ src/lessons/lesson-07-destructuring-assignment​/lesson-07.test.ts
@@ -1,11 +1,10 @@
 import {ManType} from "./lesson-07";
 
-
 let props: ManType;
-beforeEach( () => {
+beforeEach(() => {
     props = {
         name: "Sanya",
-        age:31,
+        age: 31,
         lessons: [{title: "1"}, {title: "2"}],
         address: {
             street: {
@@ -30,5 +29,15 @@ test("", () => {
 })
 
 test("", () => {
+    const l1 = props.lessons[0];
+    const l2 = props.lessons[1];
 
+    const [ls1, ls2] = props.lessons;
+
+    expect(l1.title).toBe('1')
+    expect(l2.title).toBe('2')
+
+    expect(ls1.title).toBe('1')
+    expect(ls2.title).toBe('2')
 })
+
