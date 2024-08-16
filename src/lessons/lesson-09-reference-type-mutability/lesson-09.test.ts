230e85d39305530ce861dcf133d2b4f69c8f09f9
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-08-16 17:47:55 +0300

    lesson-09.3 'reference array test

--- src/lessons/lesson-09-reference-type-mutability/lesson-09.test.ts
+++ src/lessons/lesson-09-reference-type-mutability/lesson-09.test.ts
@@ -6,8 +6,9 @@ function icreaseAge(u: UserType) {
 type UserType = {
     name: string
     age: number
+    address?: {title: string}
 }
-
+//1
 test('reference test type', () => {
 
     let user: UserType = {
@@ -26,7 +27,7 @@ test('reference test type', () => {
     expect(user.age).toBe(1000)
 })
 
-
+//2
 test('array test', () => {
 
     let users = [
@@ -47,3 +48,77 @@ test('array test', () => {
     expect(users[2]).toEqual({name: 'Bandyugan', age: 10})
 
 })
+
+//3
+test('value type test', () => {
+    let usersCount = 100;
+
+    let adminsCount = usersCount
+
+    adminsCount = adminsCount + 1;
+
+    const users = [
+        { name: 'Vasya', age: 25 },
+        { name: 'Petya', age: 30 },
+        { name: 'Bandyugan', age: 10 }
+    ];
+
+    expect(users[2]).toEqual({name: 'Bandyugan', age: 10})
+
+})
+
+//4
+test('reference test type', () => {
+    const address = {
+        title: 'Minsk'
+    }
+
+    let user: UserType = {
+        name: 'Dimych',
+        age: 32,
+        address: address
+    }
+
+    let addr = user.address
+
+    let user2: UserType = {
+        name: 'Natasha',
+        age: 30,
+        address: address
+    }
+
+    address.title = 'Minsk City'
+
+    expect(user.address).toBe(user2.address)
+    expect(user.address.title).toBe('Minsk City')
+})
+
+//5
+test('reference array test', () => {
+    const address = {
+        title: 'Minsk'
+    }
+
+    let user: UserType = {
+        name: 'Dimych',
+        age: 32,
+        address: address
+    }
+
+    let user2: UserType = {
+        name: 'Natasha',
+        age: 30,
+        address: address
+    }
+
+    const users = [ user, user2, {name: 'Misha', age: 4, address: address}]
+
+    const admins = [user, user2]
+
+    admins[0].name = 'Dmitry'
+
+    expect(users[0].name).toBe('Dmitry')
+    expect(user.name).toBe('Dmitry')
+})
+
+
