4a1269b87ddf7cfd76b752e7d82e6aac6ec87657
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-08-22 16:18:38 +0300

    lesson-10.2 immutability, deep copying

--- src/lessons/lesson-10-immutability/lesson-10.test.ts
+++ src/lessons/lesson-10-immutability/lesson-10.test.ts
@@ -1,14 +1,4 @@
-import { UserType } from './lesson-10';
-
-function makeHairStyle(u: UserType, power: number) {
-    const copy = {
-        ...u,
-        hair: u.hair / power
-    }
-    //copy.hair = u.hair / power
-
-    return copy
-}
+import {makeHairStyle, moveUser, upgradeUserLaptop, UserType, UserWithLaptopType} from './lesson-10';
 
 //1
 
@@ -16,14 +6,58 @@ test("Reference type test", () => {
     let user: UserType = {
         name: "Alex",
         hair: 32,
-        address: { title: "Minsk" },
+        address: {city: "Minsk"},
     };
 
     const awesomeUser = makeHairStyle(user, 2);
 
     expect(awesomeUser.hair).toBe(16);
     expect(user.hair).toBe(32);
+    expect(awesomeUser.address).toBe(user.address);
 });
 
 //2
 
+test("change address", () => {
+    let user: UserWithLaptopType = {
+        name: "Alex",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+    };
+
+    const movedUser = moveUser(user, 'Kiev');
+
+    expect(user).not.toBe(movedUser);
+    expect(user.address).not.toBe(movedUser.address);
+    expect(user.laptop).toBe(movedUser.laptop);
+    expect(movedUser.address.city).toBe('Kiev');
+});
+
+//3
+
+test("upgrade laptop to macbook", () => {
+    let user: UserWithLaptopType = {
+        name: "Alex",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+    };
+
+    const upgradeUser = upgradeUserLaptop(user, 'Macbook');
+
+    expect(user).not.toBe(upgradeUser);
+    expect(user.laptop).not.toBe(upgradeUser.laptop);
+    expect(user.address).toBe(upgradeUser.address);
+    expect(upgradeUser.laptop.title).toBe('Macbook');
+});
--- src/lessons/lesson-10-immutability/lesson-10.ts
+++ src/lessons/lesson-10-immutability/lesson-10.ts
@@ -1,5 +1,43 @@
 export type UserType = {
     name: string
     hair: number
-    address: { title: string };
+    address: { city: string, house?: number };
+}
+
+export type LaptopType = {
+    title: string
+}
+
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+export function makeHairStyle(u: UserType, power: number) {
+    const copy = {
+        ...u,
+        hair: u.hair / power
+    }
+    //copy.hair = u.hair / power
+
+    return copy
+}
+
+export function moveUser(u: UserWithLaptopType, city: string) {
+    return {
+        ...u,
+        address: {
+            ...u.address,
+            city: city
+        }
+    }
+}
+
+export function upgradeUserLaptop(u: UserWithLaptopType, title: string) {
+    return {
+        ...u,
+        laptop: {
+            ...u.laptop,
+            title: title
+        }
+    }
 }
